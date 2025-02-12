cd427be35b4658ca6312b0083b86cf53ab650e50
xsmail420 <tnknight97@gmail.com>
2025-02-12 09:35:48 +0100

    update patient controller

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -500,6 +500,7 @@ class PatientController extends Controller
      */
     public function destroy(int $id): RedirectResponse
     {
+        
         // Trouver le patient par ID
         $patient = $this->patientRepository->findWithoutFail($id);
 
@@ -510,7 +511,7 @@ class PatientController extends Controller
         }
 
         // Chercher l'ID du médecin connecté
-        $doctorId = auth()->user()->associatedDoctors->pluck('doctor_id')->toArray();
+         $doctorId = auth()->user()->getDoctorId();
 
         // Chercher l'association du patient avec ce médecin dans la table doctor_patient
         $doctorPatient = \DB::table('doctor_patients')
