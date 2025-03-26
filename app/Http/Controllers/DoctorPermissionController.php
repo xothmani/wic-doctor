747cd76340e5f65b04511a6f6ee8a27c18f40c9d
xothmani <rayenothmani@gmail.com>
2025-03-26 10:12:23 +0100

    update_permission

--- app/Http/Controllers/DoctorPermissionController.php
+++ app/Http/Controllers/DoctorPermissionController.php
@@ -22,6 +22,7 @@ class DoctorPermissionController extends Controller
         }
 
         $associatedUsers = DoctorAssociate::where('doctor_id', $doctor->id)
+            ->whereHas('user') // assure que 'user' existe
             ->with('user.roles.permissions')
             ->get();
 
