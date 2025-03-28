c36e4facd83b03c36979686a378705824a078583
samar aouadi <samaraouadi7@gmail.com>
2025-03-28 09:17:39 +0100

    test

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -419,11 +419,7 @@
 @endcan
 
 
-    <style>.nav-icon {
-    width: 1.25rem; /* Assurez-vous que toutes les icônes ont la même largeur */
-    text-align: center;
-}
-</style>        @can('chatTE.index')
+      @can('chatTE.index')
                 <li class="nav-item">
                     <a class="nav-link {{ Request::is('chatTE*') ? 'active' : '' }}" href="{{ url('/chatTE') }}">
                         @if($icons)
@@ -436,6 +432,11 @@
         </ul>
     </li>
 @endcan
+<style>.nav-icon {
+    width: 1.25rem; /* Assurez-vous que toutes les icônes ont la même largeur */
+    text-align: center;
+}
+</style>  
 <!-- @can('addresses.index')
     <li class="nav-item">
         <a class="nav-link {{ Request::is('addresses*') ? 'active' : '' }}"
