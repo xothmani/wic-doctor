be9616079b8fd2cb99563d8fee4ea794b003919e
Chaima-Razzeg <guiraschaima5@gmail.com>
2025-05-07 12:25:34 +0100

    numFiche

--- resources/views/patients/fields.blade.php
+++ resources/views/patients/fields.blade.php
@@ -82,7 +82,11 @@
         <div class="form-group align-items-baseline d-flex flex-column flex-md-row">
             {!! Form::label('numFiche', 'Numéro de fiche', ['class' => 'col-md-3 control-label text-md-right mr-2']) !!}
             <div class="col-md-9">
-                {!! Form::text('numFiche', $fiche->numFiche, ['class' => 'form-control', 'readonly']) !!}
+                {!! Form::text('numFiche', $fiche->numFiche, [
+                    'class' => 'form-control',
+                    'readonly',
+                    'placeholder' => 'Insérer le numéro de fiche'
+                ]) !!}
             </div>
         </div>
     @endif
@@ -90,10 +94,14 @@
     <div class="form-group align-items-baseline d-flex flex-column flex-md-row">
         {!! Form::label('numFiche', 'Numéro de fiche', ['class' => 'col-md-3 control-label text-md-right mr-2']) !!}
         <div class="col-md-9">
-            {!! Form::text('numFiche', null, ['class' => 'form-control']) !!}
+            {!! Form::text('numFiche', null, [
+                'class' => 'form-control',
+                'placeholder' => 'Insérer le numéro de fiche'
+            ]) !!}
         </div>
     </div>
 @endisset
+
 
 
 
