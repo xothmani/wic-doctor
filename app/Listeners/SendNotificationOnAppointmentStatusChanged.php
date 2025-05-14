1ea555531accfaa206fc250c574127bd8cadec1b
HamzaNechi <nechihamza114@gmail.com>
2025-05-14 15:39:52 +0100

    commit 14 mai 15h

--- app/Listeners/SendNotificationOnAppointmentStatusChanged.php
+++ app/Listeners/SendNotificationOnAppointmentStatusChanged.php
@@ -8,6 +8,8 @@ use Log;
 use App\Models\Notification;
 use App\Models\Doctor;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Lang;
 
 
 class SendNotificationOnAppointmentStatusChanged
@@ -30,13 +32,25 @@ class SendNotificationOnAppointmentStatusChanged
 
         // Assurez-vous d'avoir un moyen d'obtenir le device_token du utilisateur
         $deviceToken = $appointment->user->device_token;
+        $localUser = $appointment->user->locale_mobile;
         // Préparez la notification
 
         $doctor = Doctor::find($appointment->doctor_id);
 
-        $title = 'Appointment Status Updated';
+        //$title = 'Appointment Status Updated';
         
-        $message = "Your appointment with Dr. %s is now %s.";
+        $bdBody = "Your appointment with Dr. %s is now %s.";
+
+        Lang::setLocale($localUser ?? 'ar');
+
+        $title = $this->getCustomTranslation('Appointment Status Updated', $localUser ?? 'en');
+
+        $template = $this->getCustomTranslation('Your appointment with Dr. %s is now %s.', $localUser ?? 'en');
+        $status = $this->getCustomTranslation($this->getStatusFromId($event->status_id), $localUser ?? 'en');
+
+        $message = sprintf($template, $doctor->name, $status);
+
+
 
         // Envoi de la notification FCM
         $this->fcmService->sendNotification(
@@ -65,8 +79,21 @@ class SendNotificationOnAppointmentStatusChanged
                 'doctor_name' => $doctor->name
                 ],
             'read' => false,
-            'body' => $message
+            'body' => $bdBody
         ]);
+    }
+
+
+
+    public function getCustomTranslation($key, $locale = 'en')
+    {
+        $path = base_path("resources/lang/{$locale}/customer_app.json");
+
+        if (!file_exists($path)) return $key;
+
+        $translations = json_decode(file_get_contents($path), true);
+
+        return $translations[$key] ?? $key;
     }
 
 
