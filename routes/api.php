e76041293d2c337b4855a9aa29ee7449f33a363d
HamzaNechi <nechihamza114@gmail.com>
2025-06-09 15:40:11 +0100

    delete appoitment 9 juin - mobile

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\API\RoomAPIController;
 use App\Http\Controllers\PrescriptionController;
 use App\Http\Controllers\API\TranslationAPIController;
 use App\Http\Controllers\API\AvailabilityHourAPIController;
+use App\Http\Controllers\API\AppointmentAPIController ;
 use App\Http\Controllers\API\UserAPIController;
 use Google\Service\Doubleclicksearch\Availability;
 
@@ -38,7 +39,7 @@ Route::middleware('auth:api')->post('/set-locale', [TranslationAPIController::cl
 Route::get('/get-type-consultation/{id}', [AvailabilityHourAPIController::class, 'getTypeConsultation']);
 Route::put('update-user-email/{id}', [UserAPIController::class, 'updateUserEmail']);
 Route::get('/consultations/prescriptions/{id}', [PrescriptionsApiController::class, 'show']);
-
+Route::delete('appointments/delete/{id}', [AppointmentApiController::class, 'delete']);
 /*********************** End Route ajouté par Hamza ********************* */
 
 
