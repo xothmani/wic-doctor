83f89719ffd3a9d76703709b4c441ae03f5712ac
HamzaNechi <nechihamza114@gmail.com>
2025-07-11 14:47:10 +0100

    dme - 11 juillet 14h

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,7 @@ Route::delete('appointments/delete/{id}', [AppointmentApiController::class, 'del
 Route::post('/media/upload', [CustomMediaApiController::class, 'upload']);
 Route::delete('/media/delete/{id}', [CustomMediaApiController::class, 'destroy']);
 Route::delete('doctor_reviews/{id}', 'API\DoctorReviewAPIController@delete');
+Route::get('get-user/{id}', 'API\UserAPIController@getUser');
 /*********************** End Route ajouté par Hamza ********************* */
 
 
