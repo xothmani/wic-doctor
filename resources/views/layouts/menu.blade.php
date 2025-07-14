e1e530c8a883f1f6205c4314f0dae8988c116249
xothmani <rayenothmani@gmail.com>
2025-07-14 10:50:33 +0100

    new badge

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -574,6 +574,10 @@
         <p>
             WIC Messenger
             <span class="right badge badge-primary" id="messenger-unread-count" style="display: none;">0</span>
+            {{-- Badge NEW animé --}}
+            <span class="badge badge-new">
+                NEW
+            </span>
         </p>
     </a>
 </li>
